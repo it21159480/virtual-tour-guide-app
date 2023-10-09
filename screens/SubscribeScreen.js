34c1263b69b75c36e98d373c084adb6aa699e95a
it21159480 <muhammedmufeel876@gmail.com>
2023-10-09 18:05:01 +0530

    impelemention of rating system

--- screens/SubscribeScreen.js
+++ screens/SubscribeScreen.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet, Pressable, Image, TextInput } from 'react-native';
-import { validateEmail, validateLenght } from '../utils';
+import { Avatar, Rating } from 'react-native-elements';
 
-const SubscribeScreen = () => {
+const SubscribeScreen = ({ route }) => {
+  const { selectedRating } = route.params;
 
   const [review, onChangeReview] = useState('');
-  // const isEmailValid = validateEmail(email);
-  // const isLenghtValid = validateLenght(lenght);
+
+  function isEmpty() {
+    return review.trim() === '';
+  }
 
   const Button = ({ onPress, children, disabled }) => {
     return (
@@ -20,46 +23,42 @@ const SubscribeScreen = () => {
     );
   };
 
-  function isEmpty (){
-      if (review.trim() === '') {
-          return true;
-      }else {
-        return false;
-      }
-    }
-  
-
-
-  // Add subscribe screen code here
   return (
     <View style={styles.container}>
-
-      <Image
-        style={styles.image}
-        source={require('../assets/little-lemon-logo-grey.png')}
-        resizeMode='center'
-        accessible={true}
-        accessibilityLabel={'Little Lemon Logo'} />
-      {/* <Text style={styles.message}>Subscribe to our newsletter for our latest delicious recipes!</Text> */}
-
-      <View style={{flex:1, flexDirection:'row',justifyContent:'space-between'}} >
-        <TextInput
-          style={styles.inputBox}
-          value={review}
-          onChangeText={onChangeReview}
-          placeholder={'Add your review...'}
-          keyboardType={'ascii-capable'}
-          multiline={true}
-          numberOfLines={10}
+      <View>
+        <Text style={{textAlign:'center'}}>YOUR RATING:</Text>
+        <Rating
+          imageSize={30}
+          readonly
+          showRating
+          startingValue={selectedRating} // Use the selectedRating passed from the previous screen
+          ratingTextColor={'#004225'}
         />
-        <Button
-          onPress={() => {
-            alert("Thanks for your valuble review, stay tuned!");
-          }}
-          disabled={isEmpty()}
-        >
-         Send
-        </Button>
+        
+      </View>
+      
+      <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between' }}>
+        <View style={{ justifyContent: 'flex-end' }}>
+          <TextInput
+            style={styles.inputBox}
+            value={review}
+            onChangeText={onChangeReview}
+            placeholder={'Add your review...'}
+            keyboardType={'ascii-capable'}
+            multiline={true}
+            numberOfLines={10}
+          />
+        </View>
+        <View style={{ justifyContent: 'flex-end' }}>
+          <Button
+            onPress={() => {
+              alert("Thanks for your valuable review, stay tuned!");
+            }}
+            disabled={isEmpty()}
+          >
+            Send
+          </Button>
+        </View>
       </View>
     </View>
   );
@@ -67,25 +66,21 @@ const SubscribeScreen = () => {
 
 export default SubscribeScreen;
 
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'row',
+    flexDirection: 'colmn',
     paddingHorizontal: 20,
     paddingVertical: 20,
-   },
+    justifyContent: 'space-between',
+    backgroundColor:'white'
+  },
   image: {
     width: 150,
     height: 200,
     alignSelf: 'center'
   },
-  // message: {
-  //   fontSize: 20,
-  //   textAlign: 'center',
-  //   paddingTop: 30,
-  //   paddingBottom: 20
-  // },
+ 
   inputBox: {
     height: 80,
     width: 250,
@@ -95,28 +90,28 @@ const styles = StyleSheet.create({
     fontSize: 16,
     borderColor: 'blue',
     backgroundColor: '#EDEFEE',
-    marginBottom: 30,
-    textAlignVertical:'top'
+    marginBottom: 0,
+    textAlignVertical: 'top'
   },
   buttonContainer: {
-    height:40,
-    width:60,
-   marginTop:40,
+    height: 40,
+    width: 60,
+    marginTop: 40,
     borderRadius: 8,
     backgroundColor: 'blue',
     flexDirection: 'column',
     justifyContent: 'center',
     padding: 8,
-    
+
   },
   disabled: {
     backgroundColor: 'grey',
     opacity: 0.5,
-    
+
   },
   btn: {
     fontSize: 16,
     color: 'white',
-    textAlign:'center'
+    textAlign: 'center'
   }
 })
